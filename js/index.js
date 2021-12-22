34459fc4b2e9386a842e8895af7aa9b15feb4089
PolinaMalaeva <malaeva.polina@mail.ru>
2021-12-22 18:14:06 +0300

    modal window

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 $(document).on('click', '.showModal', function (event) {
     event.preventDefault();
     $('.modal_window').show();
+    $('body').css('overflow', 'hidden')
 });
 
 $(document).on('click', '.modal_window .close', function () {
     $('.modal_window').hide();
+    $('body').css('overflow', 'auto')
 });
 
 $(function() {
